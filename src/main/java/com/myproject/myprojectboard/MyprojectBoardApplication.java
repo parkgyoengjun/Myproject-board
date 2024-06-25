e78a5ee3c8c2d3875901bb18383c7857eda9751c
Gyeong Jun <qrw2010@naver.com>
2024-06-25 17:55:44 +0900

    #3 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    들어간 기술:
    스프링 웹, 엑츄에이터, 데브튤즈, 롬복

---
+++ src/main/java/com/myproject/myprojectboard/MyprojectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.myprojectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyprojectBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyprojectBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myproject/myprojectboard/MyprojectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.myprojectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyprojectBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
