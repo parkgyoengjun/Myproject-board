afc8265cefb35fa1567e1dcdedf6957563917b4b
Gyeong Jun <qrw2010@naver.com>
2024-07-04 20:01:56 +0900

    #9 - AuditingFields 추출
    생성자, 생성일시, 수정자, 수정일시는 반복적으로
    엔티티 클래스에 들어가는 요소이고,
    도메인과 직접적인 연관이 없는 요소이므로
    추출이 가능하다
    @MappedSuperClass 이용해서 상속 방식으로 추출
    그외 @DateTimeFormat 요소 추가 및 jpa 옵션 개선

--- src/main/java/com/myproject/myprojectboard/domain/ArticleComment.java
+++ src/main/java/com/myproject/myprojectboard/domain/ArticleComment.java
@@ -13,6 +13,8 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
+
+//@EntityListeners(AuditingEntityListener.class)
 @Getter // 롬복 라이브러리에서 제공하는 getxx() 를 자동으로 생성해줌
 @ToString // 자동으로 toString() 메서드를 생성해줌
 @Table(indexes = {
@@ -20,9 +22,8 @@ import java.util.Objects;
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy"),
 }) // Entity 와 매핑할 테이블을 지정(설정의개념)
-@EntityListeners(AuditingEntityListener.class)
 @Entity// JPA를 사용해 테이블과 매핑할 클래스에 붙여서 JPA가 관리하게한다.(선언의개념)(주의사항 : 1. 기본생성자 꼭 필요 2. final, enum, interface, innter class 사용불가 3. 필드(변수)를 final로 선언 불가 )
-public class ArticleComment { // 댓글
+public class ArticleComment extends AuditingFields{ // 댓글
 
     @Id // 특정 속성을 기본키로 설정
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본값을 자동으로 DB에 생성함, GenerationType.IDENTITY : 기본키 생성을 DB에 위임
@@ -31,11 +32,12 @@ public class ArticleComment { // 댓글
     @Setter @ManyToOne(optional = false) private Article article; // 게시글 (id)
     @Setter @Column(nullable = false, length = 500) private String content; // 본문
 
+/* AuditingFields 로 빼줌
     @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; // 생성일시
     @CreatedBy @Column(nullable = false, length = 100) private String createdBy; // 생성자
     @LastModifiedDate @Column(nullable = false, length = 100) private LocalDateTime modifiedAt; // 수정일시
     @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy; // 수정자
-
+*/
     protected ArticleComment() {}
 
     private ArticleComment(Article article, String content) {
